697afe116c1923cc02cdf4f9f3a035c55fa8cdf3
ZayYarNaing <zayyarnaing@aceplussolutions.com>
2024-06-25 17:15:52 +0630

    Added Login.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,9 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import LoginForm from "./pages/login/Login";
 import Router from "./routes/Router";
 
 function App() {
   return (
     <>
-      {/* <div className="App">
-        <LoginForm />
-      </div> */}
       <Router />
     </>
   );
---
+++ src/config/axios.ts
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+export const baseURL = import.meta.env.VITE_BASE_URL;
+
+
+const api = axios.create({
+    baseURL: import.meta.env.VITE_BASE_URL,
+    headers: {
+      contentType: "application/json",
+      "Cache-Control": "no-cache",
+      "Access-Control-Allow-Origin": "*",
+      Accept: "application/json",
+    },
+  });
